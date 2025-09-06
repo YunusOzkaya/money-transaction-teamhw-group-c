3e43e9ddb12d4b3ffbac87670ddec068c5e675a8
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-09-06 17:14:23 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -4,21 +4,16 @@ import {
   logoutThunk,
   refreshUserThunk,
   registerThunk,
-  editUserName,
-  editUserAvatar,
   getTotalBalanceThunk,
-  resetPassword,
-  changePassword
 } from "./operations";
 
 const initialState = {
   user: {
-    name: null,
+    id: null,
+    username: null,
     email: null,
     balance: null,
-    avatar: null,
   },
-  preview: null,
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
@@ -34,12 +29,6 @@ const authSlice = createSlice({
     updateBalance: (state, action) => {
       state.user.balance = action.payload;
     },
-    setAvatarPreview: (state, action) => {
-      state.preview = action.payload;
-    },
-    clearAvatarPreview: (state) => {
-      state.preview = null;
-    },
   },
   extraReducers: (builder) => {
     builder
@@ -50,7 +39,7 @@ const authSlice = createSlice({
         state.user.balance = action.payload;
       })
       .addCase(refreshUserThunk.fulfilled, (state, action) => {
-        state.user = action.payload.data;
+        state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
         state.isAuthLoading = false;
@@ -65,52 +54,14 @@ const authSlice = createSlice({
         state.isAuthLoading = false;
         state.isLoggedIn = false;
       })
-      .addCase(editUserName.fulfilled, (state, action) => {
-        state.user.name = action.payload.data.name;
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-        state.isAuthLoading = false;
-      })
-      .addCase(editUserAvatar.fulfilled, (state, action) => {
-        state.user.avatar = action.payload.data.avatar;
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-        state.isAuthLoading = false;
-      })
-      .addCase(resetPassword.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-        state.resetEmailSent = false;
-      })
-      .addCase(resetPassword.fulfilled, (state) => {
-        state.isLoading = false;
-        state.resetEmailSent = true;
-      })
-      .addCase(resetPassword.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-      .addCase(changePassword.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-        state.passwordChanged = false;
-      })
-      .addCase(changePassword.fulfilled, (state) => {
-        state.isLoading = false;
-        state.passwordChanged = true;
-      })
-      .addCase(changePassword.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
       .addMatcher(
         isAnyOf(registerThunk.fulfilled, loginThunk.fulfilled),
         (state, action) => {
-          state.user.name = action.payload.data.user.name;
-          state.user.email = action.payload.data.user.email;
-          state.user.balance = action.payload.data.user.balance;
-          state.user.avatar = action.payload.data.user.avatar;
-          state.token = action.payload.data.accessToken;
+          state.user.id = action.payload.user.id;
+          state.user.username = action.payload.user.username;
+          state.user.email = action.payload.user.email;
+          state.user.balance = action.payload.user.balance;
+          state.token = action.payload.token;
           state.isLoggedIn = true;
           state.isRegistering = false;
         }
@@ -135,5 +86,4 @@ const authSlice = createSlice({
 });
 
 export const authReducer = authSlice.reducer;
-export const { updateBalance, setAvatarPreview, clearAvatarPreview } =
-  authSlice.actions;
+export const { updateBalance } = authSlice.actions;
