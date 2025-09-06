1fbef4ecd5637641f7cbe2cc13448345f95eeda0
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-09-06 17:17:33 +0300

    Update selectors.js

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,3 +1,5 @@
 export const selectTransactions = (state) => state.transactions.transactions;
+export const selectCategories = (state) => state.transactions.categories;
+export const selectSummary = (state) => state.transactions.summary;
 export const isTransLoading = (state) => state.transactions.isTransLoading;
 export const isTransError = (state) => state.transactions.isTransError;
